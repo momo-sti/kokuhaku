6182ad68d30e4dd3aad1b85581a8e60a06b7e970
momo-sti <momoka1112sti@gmail.com>
2023-08-17 00:08:46 +0900

    Fix:バリデーション修正

--- app/models/confession.rb
+++ app/models/confession.rb
@@ -2,5 +2,5 @@ class Confession < ApplicationRecord
   has_one :reply
   
   validates :username, presence: true, length: { maximum: 10 }
-  validates :text, presence: true, length: { maximum: 30 }
+  validates :text, presence: true, length: { maximum: 50 }
 end
